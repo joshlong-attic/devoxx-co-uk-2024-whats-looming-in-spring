13db1ea8d75373c9ab661f9edf38d14f6a5a69de
Josh Long <josh@joshlong.com>
2024-05-02 01:36:56 -0700

    polish

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -50,7 +50,8 @@ cd $GITHUB_WORKSPACE
 # todo
 #./mvnw --batch-mode --no-transfer-progress -DskipTests -Pnative native:compile
 #docker build . -f $GITHUB_WORKSPACE/deploy/Dockerfile  -t $IMAGE_NAME --build-arg APP_NAME=$APP_NAME
-#docker push $IMAGE_NAME
+./mvnw -DskipTests spring-boot:build-image -DimageName=$IMAGE_NAME
+docker push $IMAGE_NAME
 ## /todo
 
 Y=app-${APP_NAME}-data.yml
