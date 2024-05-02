43df4a4dd74e83b23db8eb96375bb15861d75b73
Josh Long <josh@joshlong.com>
2024-05-02 12:03:17 -0700

    deploy

--- deploy/write_ips.sh
+++ deploy/write_ips.sh
@@ -2,6 +2,7 @@
 
 IPS_FN=$GITHUB_WORKSPACE/deploy/IPS
 kubectl get services -o json |  jq -r  '.items[].spec.selector.app' | while read svc; do
+  echo "svc is  $svc "
   IP="$( kubectl  get services/${svc}-service -o json | jq -r '.status.loadBalancer.ingress.[0].ip' )"
   echo $IP >> $IPS_FN
 done
