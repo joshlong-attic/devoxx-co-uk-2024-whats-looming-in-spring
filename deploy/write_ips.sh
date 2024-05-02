66614929b271b7e714991137ba15cda6d699fe30
Josh Long <josh@joshlong.com>
2024-05-02 15:35:24 -0700

    forgot to apply the right pod descriptor

--- deploy/write_ips.sh
+++ deploy/write_ips.sh
@@ -7,7 +7,7 @@ touch $IPS_FN
 kubectl get services -o json |  jq -r  '.items[].spec.selector.app' | while read svc; do
   echo "svc is $svc "
   kubectl  get services/${svc}-service -o json
-  kubectl  get services/${svc}-service -o json | jq -r '.status.loadBalancer.ingress.[0].ip'  > IP
+  kubectl  get services/${svc}-service -o json | jq -r ".status.loadBalancer.ingress.[].ip"  > IP
   IP=`cat IP`
   echo $IP >> $IPS_FN
 done
