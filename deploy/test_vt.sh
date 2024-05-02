7074adb173275d0429cf98262da8fc50e9506381
Josh Long <josh@joshlong.com>
2024-05-02 11:39:25 -0700

    forgot to apply the right pod descriptor

--- deploy/test_vt.sh
+++ deploy/test_vt.sh
@@ -10,7 +10,9 @@ cat IPS | while read IP ; do
   echo "the URI is $URI and $VT "
   for URL_PATH in jdbc memory-no-sleep memory-sleep ; do
     for i in {1..5}; do
-      ab -c 100 -n 1000 ${URI}/${URL_PATH} > ${OUTPUT_DIR}/${URL_PATH}-${VT}
+
+      echo ab -c 100 -n 1000 ${URI}/${URL_PATH}
+      #> ${OUTPUT_DIR}/${URL_PATH}-${VT}
     done
   done
 done
