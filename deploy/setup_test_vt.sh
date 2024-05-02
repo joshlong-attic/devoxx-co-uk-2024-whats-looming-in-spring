ab8a9d41087d557e8ea652f4b192cd88f4ef7a98
Josh Long <josh@joshlong.com>
2024-05-02 05:02:10 -0700

    polish

--- deploy/setup_test_vt.sh
+++ deploy/setup_test_vt.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
 # Install necessary packages
-apt-get update && apt-get install -y \
-    zsh \
-    curl \
-    jq \
-    gnupg \
-    lsb-release \
-    && rm -rf /var/lib/apt/lists/*
+apt-get update && apt-get install -y zsh curl jq gnupg lsb-release && rm -rf /var/lib/apt/lists/*
 
-## Install kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" > /usr/src/app
 
-
-# Install Rust and then oha
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-export PATH="/root/.cargo/bin:${PATH}"
+
 cargo install oha
