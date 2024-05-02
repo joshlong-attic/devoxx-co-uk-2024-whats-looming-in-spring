b65816774d95f5d60021a8ccd3024f46099b96b9
Josh Long <josh@joshlong.com>
2024-05-02 06:09:27 -0700

    polish

--- deploy/test_vt.sh
+++ deploy/test_vt.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+OUTPUT_DIR=$HOME/Desktop/tests
+
+mkdir -p $OUTPUT_DIR
+
 cat IPS | while read IP ; do
   URI=http://${IP}:8080
   VT=$( curl ${URI}/threads | jq  -r '.["spring.threads.virtual.enabled"]' )
   echo "the URI is $URI and $VT "
-  OUTPUT_DIR=$HOME/Desktop/tests
-  mkdir -p $OUTPUT_DIR
   for URL_PATH in jdbc memory-no-sleep memory-sleep ; do
     for i in {1..5}; do
       ab -c 100 -n 1000 ${URI}/${URL_PATH} > ${OUTPUT_DIR}/${URL_PATH}-${VT}
--- deploy/write_ips.sh
+++ deploy/write_ips.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-IPS_FN=IPS
+IPS_FN=$GITHUB_WORKSPACE/deploy/IPS
 kubectl get services -o json |  jq -r  '.items[].spec.selector.app' | while read svc; do
   IP="$( kubectl  get services/${svc}-service -o json | jq -r '.status.loadBalancer.ingress.[0].ip' )"
   echo $IP >> $IPS_FN
